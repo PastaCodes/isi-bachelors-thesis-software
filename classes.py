Add back ephemeris, use correct covariance matrices

classes.py
@@ -1,4 +1,4 @@
-from typing import Collection
+from functools import cached_property
 
 import numpy as np
 from astropy.coordinates import EarthLocation
@@ -15,11 +15,29 @@ class Observatory:
 
 
 class MinorPlanet:
-    def __init__(self, designations: str | Collection[str], observations_filepath: str | None = None,
-                 ephemeris_filepath: str | None = None):
-        self.designations = designations if isinstance(designations, list) else [designations]
+    def __init__(self, display_name: str | None = None, jpl_designation: str | None = None,
+                 additional_designations: str | set[str] | None = None,
+                 observations_filepath: str | None = None, ephemeris_filepath: str | None = None):
+        self.display_name = jpl_designation if display_name is None else display_name
+        self.jpl_designation = display_name if jpl_designation is None else jpl_designation
+        if isinstance(additional_designations, set):
+            self.additional_designations = additional_designations
+        elif isinstance(additional_designations, str):
+            self.additional_designations = {additional_designations}
+        else:
+            self.additional_designations = set()
         self.observations_filepath = observations_filepath
         self.ephemeris_filepath = ephemeris_filepath
+
+    @cached_property
+    def all_designations(self):
+        all_designations = set()
+        if self.display_name:
+            all_designations.add(self.display_name)
+        if self.jpl_designation:
+            all_designations.add(self.jpl_designation)
+        all_designations.update(self.additional_designations)
+        return all_designations
 
 
 class MinorPlanetObservation:
@@ -61,55 +79,30 @@ class MinorPlanetState:
         self.slope_parameter = gg
 
 
-'''
 class MinorPlanetEphemeris:
-    def __init__(self, target_body: MinorPlanet, epoch: Time, target_pos: CartesianRepresentation,
-                 observer_loc: EarthLocation, observer_pos: CartesianRepresentation, sun_pos: CartesianRepresentation,
-                 a: Quantity, e: Quantity, i: Quantity, n: Quantity, asc_long: Quantity, peri_arg: Quantity,
-                 mm: Quantity, v: Quantity, ra: Quantity, dec: Quantity, app_ra: Quantity, app_dec: Quantity,
-                 vv: Quantity, th: Quantity, phi: Quantity):
-        """
-        :param target_body:
-        :param epoch:
-        :param target_pos: Geometric position of the target's barycenter
-        :param observer_loc: Earth location of the observer
-        :param observer_pos: Geometric position of the observer
-        :param sun_pos: Geometric position of the Sun's barycenter
-        :param a:
-        :param e:
-        :param i:
-        :param n:
-        :param asc_long:
-        :param peri_arg:
-        :param mm:
-        :param v:
-        :param ra: Astrometric right ascension
-        :param dec: Astrometric declination
-        :param app_ra: Refracted apparent right ascension
-        :param app_dec: Refracted apparent declination
-        :param vv:
-        :param th:
-        :param phi: True phase
-        """
+    def __init__(self, target_body: MinorPlanet, epoch: Time, target_pos: np.ndarray,
+                 observer_pos: np.ndarray, sun_pos: np.ndarray,
+                 a: float, e: float, i: float, n: float, om: float, w: float,
+                 mm: float, v: float, astro_ra: float, astro_dec: float, app_ra: float, app_dec: float,
+                 app_vv: float, app_th: float, app_phi: float, astro_phi: float):
         self.target_body = target_body
         self.epoch = epoch
         self.target_position = target_pos
-        self.observer_location = observer_loc
         self.observer_position = observer_pos
         self.sun_position = sun_pos
         self.semi_major_axis = a
         self.eccentricity = e
         self.inclination = i
         self.mean_motion = n
-        self.ascending_longitude = asc_long
-        self.periapsis_argument = peri_arg
+        self.ascending_longitude = om
+        self.periapsis_argument = w
         self.mean_anomaly = mm
         self.true_anomaly = v
-        self.right_ascension = ra
-        self.declination = dec
+        self.astrometric_right_ascension = astro_ra
+        self.astrometric_declination = astro_dec
         self.apparent_right_ascension = app_ra
         self.apparent_declination = app_dec
-        self.visual_magnitude = vv
-        self.elongation = th  # Not quite the true elongation
-        self.phase = phi
-'''
+        self.apparent_visual_magnitude = app_vv
+        self.apparent_elongation = app_th
+        self.apparent_phase = app_phi
+        self.astrometric_phase = astro_phi

filter.py
@@ -3,17 +3,23 @@ from filterpy.kalman import MerweScaledSigmaPoints, UnscentedKalmanFilter
 
 from classes import MinorPlanet
 from misc import get_sun_position, get_location_position
-from model import propagate, measure, finalize_transform, initial_state
+from model import propagate, measure, finalize_transform, initial_state, state_autocovariance_matrix, \
+    measurement_autocovariance_matrix
 from orbit import get_mean_motion
 from parse import parse_observations
 
 
-def do_filter(body: MinorPlanet, dir_conc: float, vv_var: float,
-              a0: float = 1.0, e0: float = 0.5, i0: float = 0.25 * np.pi, n0: float | None = None,
-              hh0: float = 20.0, gg0: float = 0.15, mm0_hint: float = 0.0, om0_hint: float = 0.0) -> None:
-    obss = parse_observations(body)
+def do_filter(body: MinorPlanet, a0: float = 1.0, e0: float = 0.5, i0: float = 0.25 * np.pi, n0: float | None = None,
+              hh0: float = 20.0, gg0: float = 0.15, mm0_hint: float = 0.0, om0_hint: float = 0.0,
+              mm0_var: float = 1E-3, a0_var: float = 1E-3, e0_var: float = 1E-3, i0_var: float = 1E-3,
+              om0_var: float = 1E-3, w0_var: float = 1E-3, n0_var: float = 1E-3,
+              hh0_var: float = 1E-3, gg0_var: float = 1E-3,
+              mm_var: float = 1E-5, a_var: float = 1E-7, e_var: float = 1E-7, i_var: float = 1E-7,
+              om_var: float = 1E-7, w_var: float = 1E-7, n_var: float = 1E-7,
+              hh_var: float = 1E-7, gg_var: float = 1E-7, dir_var: float = 1E-5, vv_var: float = 1E-2) -> None:
+    obss = list(parse_observations(body))
 
-    points = MerweScaledSigmaPoints(n=13, alpha=.1, beta=2., kappa=-1)
+    points = MerweScaledSigmaPoints(n=13, alpha=.1, beta=2., kappa=0)
     ukf = UnscentedKalmanFilter(dim_x=13, dim_z=5, dt=None, hx=measure, fx=propagate, points=points)
 
     if n0 is None:
@@ -25,35 +31,21 @@ def do_filter(body: MinorPlanet, dir_conc: float, vv_var: float,
                        get_location_position(obs0.observatory.location, t0),
                        get_sun_position(t0),
                        a0, e0, i0, n0, hh0, gg0, mm0_hint, om0_hint)
-    pp0 = np.diag([0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1])
-    prev_epoch = t0
+    pp0 = state_autocovariance_matrix(x0, mm0_var, a0_var, e0_var, i0_var, om0_var, w0_var, n0_var, hh0_var, gg0_var)
+    prev_epoch = t0.tdb
     ukf.x = x0
     ukf.P = pp0
 
     for i, obs in enumerate(obss[1:]):
         obs_vec = obs.to_vector()
-        cos_ra, sin_ra, cos_dec, sin_dec, vv = obs_vec
-        dt = (obs.epoch - prev_epoch).to_value('jd')
-        ukf.Q = np.diag([0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1])
-        ukf.R = np.array([[sin_ra * sin_ra / (dir_conc * cos_dec * cos_dec),
-                           -sin_ra * cos_ra / (dir_conc * cos_dec * cos_dec),
-                           0.0, 0.0, 0.0],
-                          [-sin_ra * cos_ra / (dir_conc * cos_dec * cos_dec),
-                           cos_ra * cos_ra / (dir_conc * cos_dec * cos_dec),
-                           0.0, 0.0, 0.0],
-                          [0.0, 0.0,
-                           sin_dec * sin_dec / dir_conc,
-                           -sin_dec * cos_dec / dir_conc,
-                           0.0],
-                          [0.0, 0.0,
-                           -sin_dec * cos_dec / dir_conc,
-                           cos_dec * cos_dec / dir_conc,
-                           0.0],
-                          [0.0, 0.0, 0.0, 0.0, vv_var]])
+        epoch = obs.epoch.tdb
+        dt = (epoch - prev_epoch).to_value('jd')
+        ukf.Q = state_autocovariance_matrix(ukf.x, mm_var, a_var, e_var, i_var, om_var, w_var, n_var, hh_var, gg_var)
         ukf.predict(dt=dt)
-        sun_pos = get_sun_position(obs.epoch)
-        obs_pos = get_location_position(obs.observatory.location, obs.epoch)
+        sun_pos = get_sun_position(epoch)
+        obs_pos = get_location_position(obs.observatory.location, epoch)
+        ukf.R = measurement_autocovariance_matrix(obs_vec, dir_var, vv_var)
         ukf.update(z=obs_vec, obs_pos=obs_pos, sun_pos=sun_pos)
         print(finalize_transform(ukf.x))
         input()
-        prev_epoch = obs.epoch
+        prev_epoch = epoch

parse.py
@@ -1,9 +1,10 @@
-from typing import Final, Collection
+from typing import Final, Collection, Iterable
 
 import numpy as np
+from astropy.time import Time
 from fortranformat import FortranRecordReader
 
-from classes import MinorPlanetObservation, MinorPlanet, Observatory
+from classes import MinorPlanetObservation, MinorPlanet, Observatory, MinorPlanetEphemeris
 from misc import location_from_parallax, decimal_day_date_to_time, PROJECT_ROOT
 
 
@@ -42,10 +43,9 @@ def parse_observation_line(body: MinorPlanet, line: str) -> MinorPlanetObservati
 
 
 def parse_observations(body: MinorPlanet,
-                       accept_methods: Collection[str] | None = ('B', 'C')) -> list[MinorPlanetObservation]:
+                       accept_methods: Collection[str] | None = ('B', 'C')) -> Iterable[MinorPlanetObservation]:
     if not observatories:
         load_observatories()
-    result: list[MinorPlanetObservation] = []
     with open(PROJECT_ROOT + body.observations_filepath, 'rt', encoding='utf-8') as file:
         prev_time = None
         for line in file.readlines():
@@ -60,62 +60,22 @@ def parse_observations(body: MinorPlanet,
                 continue
             if not line[65:71].strip():
                 continue
-            result.append(obs)
+            yield obs
             prev_time = obs.epoch
-    return result
 
 
-'''
-def parse_ephemeris(body: MinorPlanet) -> dict[float, MinorPlanetEphemeris]:
-    data: dict[float, MinorPlanetEphemeris] = {}
+def parse_ephemeris(body: MinorPlanet) -> Iterable[MinorPlanetEphemeris]:
     with open(PROJECT_ROOT + body.ephemeris_filepath, 'rt', encoding='utf-8') as file:
         for line in file.readlines():
-            (t, bx, by, bz, lon, lat, h, ox, oy, oz, sx, sy, sz, a, e, i, n, asc_long, peri_arg, mm, v,
-             ra, dec, app_ra, app_dec, vv, th, phi) = line.split(' ')
-            time = Time(t, format='decimalyear', scale='utc')
-            body_pos = CartesianRepresentation(float(bx), float(by), float(bz), u.au)
-            obs_loc = EarthLocation.from_geodetic(float(lon) * u.rad, float(lat) * u.rad, float(h) * u.m)
-            obs_pos = CartesianRepresentation(float(ox), float(oy), float(oz), u.au)
-            sun_pos = CartesianRepresentation(float(sx), float(sy), float(sz), u.au)
-            data[float(t)] = MinorPlanetEphemeris(body, time, body_pos, obs_loc, obs_pos, sun_pos, float(a) * u.au,
-                                                  float(e) * u.dimensionless_unscaled, float(i) * u.rad,
-                                                  float(n) * u.rad / u.year, float(asc_long) * u.rad,
-                                                  float(peri_arg) * u.rad, float(mm) * u.rad, float(v) * u.rad,
-                                                  float(ra) * u.rad, float(dec) * u.rad, float(app_ra) * u.rad,
-                                                  float(app_dec) * u.rad, float(vv) * u.mag, float(th) * u.rad,
-                                                  float(phi) * u.rad)
-    return data
-
-
-def dump_ephemeris(ephs: dict[float, MinorPlanetEphemeris], body: MinorPlanet) -> None:
-    with open(PROJECT_ROOT + body.ephemeris_filepath, 'wt', encoding='utf-8') as file:
-        lines = []
-        for t, eph in sorted(ephs.items()):
-            bx, by, bz = eph.body_position.get_xyz().to_value(u.au)
-            lon, lat, h = eph.observer_location.geodetic
-            lon = lon.to_value(u.rad)
-            lat = lat.to_value(u.rad)
-            h = h.to(u.m).value
-            ox, oy, oz = eph.observer_position.get_xyz().to_value(u.au)
-            sx, sy, sz = eph.sun_position.get_xyz().to_value(u.au)
-            a = eph.semi_major_axis.to_value(u.au)
-            e = eph.eccentricity.value
-            i = eph.inclination.to_value(u.rad)
-            n = eph.mean_motion.to_value(u.rad / u.year)
-            asc_long = eph.ascending_longitude.to_value(u.rad)
-            peri_arg = eph.periapsis_argument.to_value(u.rad)
-            mm = eph.mean_anomaly.to_value(u.rad)
-            v = eph.true_anomaly.to_value(u.rad)
-            ra = eph.right_ascension.to_value(u.rad)
-            dec = eph.declination.to_value(u.rad)
-            app_ra = eph.apparent_right_ascension.to_value(u.rad)
-            app_dec = eph.apparent_declination.to_value(u.rad)
-            vv = eph.visual_magnitude.to_value(u.mag)
-            th = eph.elongation.to_value(u.rad)
-            phi = eph.phase.to_value(u.rad)
-            lines.append(f'{t} {bx} {by} {bz} {lon} {lat} {h} {ox} {oy} {oz} {sx} {sy} {sz} {a} {e} {i} {n} '
-                         f'{asc_long} {peri_arg} {mm} {v} {ra:.6f} {dec:.6f} {app_ra:.6f} {app_dec:.6f} '
-                         f'{vv} {th} {phi}\n')
-
-        file.writelines(lines)
-'''
+            (t, tx, ty, tz, ox, oy, oz, sx, sy, sz, a, e, i, om, w, mm, v, n, astro_ra, astro_dec,
+             app_ra, app_dec, app_vv, app_th, app_phi, astro_phi) = line.split(' ')
+            yield MinorPlanetEphemeris(body, Time(float(t), format='jd', scale='tdb'),
+                                       np.array([float(tx), float(ty), float(tz)]),
+                                       np.array([float(ox), float(oy), float(oz)]),
+                                       np.array([float(sx), float(sy), float(sz)]),
+                                       float(a), float(e), np.radians(float(i)), np.radians(float(om)),
+                                       np.radians(float(w)), np.radians(float(mm)), np.radians(float(v)),
+                                       np.radians(float(n)), np.radians(float(astro_ra)), np.radians(float(astro_dec)),
+                                       np.radians(float(app_ra)), np.radians(float(app_dec)), float(app_vv),
+                                       np.radians(float(app_th)), np.radians(float(app_phi)),
+                                       np.radians(float(astro_phi)))

query.py
@@ -1,18 +1,19 @@
 import itertools
+import urllib.parse
 from typing import Iterable
 
 import astropy.units as u
-import numpy as np
 import requests
-from astropy.coordinates import EarthLocation, CartesianRepresentation
+from astropy.coordinates import EarthLocation
 from astropy.time import Time
-from astropy.units import Quantity
 
-from classes import MinorPlanetEphemeris, MinorPlanet
+from classes import MinorPlanet
+from misc import PROJECT_ROOT
 from parse import parse_observations
 
 
-def query_vector_table(command: str, times: list[Time]) -> Iterable[CartesianRepresentation]:
+def query_target_position(command: str, times: list[Time]) -> Iterable[tuple[str, str, str]]:
+    print('Querying target position...')
     tlist = '%20'.join([f'%27{t.utc.to_value('jd')}%27' for t in times])
     url = 'https://ssd.jpl.nasa.gov/api/horizons.api' \
           '?format=text' \
@@ -33,11 +34,14 @@ def query_vector_table(command: str, times: list[Time]) -> Iterable[CartesianRep
     content = response.text.split('$$SOE\n', 1)[1].split('\n$$EOE', 1)[0]
     for line in content.split('\n'):
         columns = line.split(',')
-        yield CartesianRepresentation(float(columns[2]), float(columns[3]), float(columns[4]), u.au)
+        x = columns[2].strip()
+        y = columns[3].strip()
+        z = columns[4].strip()
+        yield x, y, z
 
 
-def query_orbital_elements(command: str, times: list[Time]) -> \
-        Iterable[tuple[Quantity, Quantity, Quantity, Quantity, Quantity, Quantity, Quantity, Quantity]]:
+def query_orbital_elements(command: str, times: list[Time]) -> Iterable[tuple[str, str, str, str, str, str, str, str]]:
+    print('Querying orbital elements...')
     tlist = '%20'.join([f'%27{t.tdb.to_value('jd')}%27' for t in times])
     url = 'https://ssd.jpl.nasa.gov/api/horizons.api' \
           '?format=text' \
@@ -57,19 +61,20 @@ def query_orbital_elements(command: str, times: list[Time]) -> \
     content = response.text.split('$$SOE\n', 1)[1].split('\n$$EOE', 1)[0]
     for line in content.split('\n'):
         columns = line.split(',')
-        e = float(columns[2]) * u.dimensionless_unscaled
-        i = float(columns[4]) * u.deg
-        asc_long = float(columns[5]) * u.deg
-        peri_arg = float(columns[6]) * u.deg
-        n = float(columns[8]) * u.deg / u.d
-        mm = float(columns[9]) * u.deg
-        v = float(columns[10]) * u.deg
-        a = float(columns[11]) * u.au
-        yield a, e, i, n, asc_long, peri_arg, mm, v
+        e = columns[2].strip()
+        i = columns[4].strip()
+        om = columns[5].strip()
+        w = columns[6].strip()
+        n = columns[8].strip()
+        mm = columns[9].strip()
+        v = columns[10].strip()
+        a = columns[11].strip()
+        yield a, e, i, om, w, mm, v, n
 
 
-def query_observer_table(command: str, times: list[Time], location: EarthLocation) -> \
-        Iterable[tuple[Quantity, Quantity, Quantity, Quantity, Quantity, Quantity, Quantity]]:
+def query_observation(command: str, times: list[Time], location: EarthLocation) -> \
+        Iterable[tuple[str, str, str, str, str, str, str, str]]:
+    print('Querying observation data...')
     tlist = '%20'.join([f'%27{t.utc.to_value('jd')}%27' for t in times])
     lon, lat, h = location.geodetic
     url = 'https://ssd.jpl.nasa.gov/api/horizons.api' \
@@ -82,7 +87,7 @@ def query_observer_table(command: str, times: list[Time], location: EarthLocatio
           f'&SITE_COORD=%27{lon.to(u.deg).value}%2C{lat.to(u.deg).value}%2C{h.to(u.km).value}%27' \
           '&TLIST_TYPE=JD' \
           '&TIME_TYPE=UT' \
-          '&QUANTITIES=%271%2C2%2C9%2C23%2C24%2C28%2C43%27' \
+          '&QUANTITIES=%271%2C2%2C9%2C23%2C24%2C43%27' \
           '&REF_SYSTEM=ICRF' \
           '&REF_PLANE=FRAME' \
           '&ANG_FORMAT=DEG' \
@@ -96,17 +101,19 @@ def query_observer_table(command: str, times: list[Time], location: EarthLocatio
     content = response.text.split('$$SOE\n', 1)[1].split('\n$$EOE', 1)[0]
     for line in content.split('\n'):
         columns = line.split(',')
-        ra = float(columns[3]) * u.deg
-        dec = float(columns[4]) * u.deg
-        app_ra = float(columns[5]) * u.deg
-        app_dec = float(columns[6]) * u.deg
-        vv = float(columns[7]) * u.mag
-        th = np.radians(float(columns[9])) * u.deg
-        phi = np.radians(float(columns[13])) * u.deg
-        yield ra, dec, app_ra, app_dec, vv, th, phi
+        astro_ra = columns[3].strip()
+        astro_dec = columns[4].strip()
+        app_ra = columns[5].strip()
+        app_dec = columns[6].strip()
+        app_vv = columns[7].strip()
+        app_th = columns[9].strip()
+        app_phi = columns[11].strip()
+        astro_phi = columns[12].strip()
+        yield astro_ra, astro_dec, app_ra, app_dec, app_vv, app_th, app_phi, astro_phi
 
 
-def query_earth_location(location: EarthLocation, times: list[Time]) -> Iterable[CartesianRepresentation]:
+def query_observer_position(location: EarthLocation, times: list[Time]) -> Iterable[tuple[str, str, str]]:
+    print('Querying observer position...')
     tlist = '%20'.join([f'%27{t.utc.to_value('jd')}%27' for t in times])
     lon, lat, h = location.geodetic
     url = 'https://ssd.jpl.nasa.gov/api/horizons.api' \
@@ -128,10 +135,14 @@ def query_earth_location(location: EarthLocation, times: list[Time]) -> Iterable
     content = response.text.split('$$SOE\n', 1)[1].split('\n$$EOE', 1)[0]
     for line in content.split('\n'):
         columns = line.split(',')
-        yield CartesianRepresentation(float(columns[2]), float(columns[3]), float(columns[4]), u.au)
+        x = columns[2].strip()
+        y = columns[3].strip()
+        z = columns[4].strip()
+        yield x, y, z
 
 
-def query_sun(times: list[Time]) -> Iterable[CartesianRepresentation]:
+def query_sun_position(times: list[Time]) -> Iterable[tuple[str, str, str]]:
+    print('Querying Sun position...')
     tlist = '%20'.join([f'%27{t.utc.to_value('jd')}%27' for t in times])
     url = 'https://ssd.jpl.nasa.gov/api/horizons.api' \
           '?format=text' \
@@ -152,32 +163,45 @@ def query_sun(times: list[Time]) -> Iterable[CartesianRepresentation]:
     content = response.text.split('$$SOE\n', 1)[1].split('\n$$EOE', 1)[0]
     for line in content.split('\n'):
         columns = line.split(',')
-        yield CartesianRepresentation(float(columns[2]), float(columns[3]), float(columns[4]), u.au)
+        x = columns[2].strip()
+        y = columns[3].strip()
+        z = columns[4].strip()
+        yield x, y, z
 
 
-def query_ephemeris(body: MinorPlanet, max_batch_size: int = 30) -> dict[float, MinorPlanetEphemeris]:
-    command = f'%27DES%3D{body.name.replace(' ', '%20')}%3B%27'
-    all_observations = parse_observations(body)
+def query_ephemeris(target_body: MinorPlanet, max_batch_size: int = 30):
+    command = f'%27DES%3D{urllib.parse.quote(target_body.jpl_designation)}%3B%27'
+    all_observations = parse_observations(target_body)
     by_observatory = itertools.groupby(all_observations, lambda obs: obs.observatory)
-    result: dict[float, MinorPlanetEphemeris] = {}
+    ephs: dict[float, tuple] = dict()
     for observatory, group in by_observatory:
         batches = itertools.batched(group, max_batch_size)
         for batch in batches:
             times = [o.epoch for o in batch]
-            for epoch, target_pos, orb_data, obs_data, loc_pos, sun_pos in \
+            for epoch, tgt_pos, orb_data, obs_data, obs_pos, sun_pos in \
                     zip(times,
-                        query_vector_table(command, times),
+                        query_target_position(command, times),
                         query_orbital_elements(command, times),
-                        query_observer_table(command, times, observatory.location),
-                        query_earth_location(observatory.location, times),
-                        query_sun(times)):
-                a, e, i, n, asc_long, peri_arg, mm, v = orb_data
-                ra, dec, app_ra, app_dec, vv, th, phi = obs_data
-                t = epoch.to_value('decimalyear')
-                result[t] = MinorPlanetEphemeris(body, epoch, target_pos, observatory.location, loc_pos, sun_pos,
-                                                 a, e, i, n, asc_long, peri_arg, mm, v, ra, dec, app_ra, app_dec,
-                                                 vv, th, phi)
+                        query_observation(command, times, observatory.location),
+                        query_observer_position(observatory.location, times),
+                        query_sun_position(times)):
+                t = epoch.tdb.to_value('jd')
+                tx, ty, tz = tgt_pos
+                a, e, i, om, w, mm, v, n = orb_data
+                astro_ra, astro_dec, app_ra, app_dec, app_vv, app_th, app_phi, astro_phi = obs_data
+                ox, oy, oz = obs_pos
+                sx, sy, sz = sun_pos
+                ephs[t] = (tx, ty, tz, ox, oy, oz, sx, sy, sz, a, e, i, om, w, mm, v, n, astro_ra, astro_dec,
+                           app_ra, app_dec, app_vv, app_th, app_phi, astro_phi)
 
-            print(f'Batch completed. Current total: {len(result)} of {len(all_observations)}')
+            print(f'Batch completed. Current total: {len(ephs)} of {len(all_observations)}')
 
-    return result
+    lines = []
+    for t, eph in sorted(ephs.items()):
+        (tx, ty, tz, ox, oy, oz, sx, sy, sz, a, e, i, om, w, mm, v, n, astro_ra, astro_dec,
+         app_ra, app_dec, app_vv, app_th, app_phi, astro_phi) = eph
+        lines.append(f'{t:.10f} {tx} {ty} {tz} {ox} {oy} {oz} {sx} {sy} {sz} {a} {e} {i} {om} {w} {mm} {v} {n} '
+                     f'{astro_ra} {astro_dec} {app_ra} {app_dec} {app_vv} {app_th} {app_phi} {astro_phi}\n')
+
+    with open(PROJECT_ROOT + target_body.ephemeris_filepath, 'wt', encoding='utf-8') as file:
+        file.writelines(lines)

simulation/ideal_model_test.py
@@ -4,7 +4,8 @@ from filterpy.kalman import MerweScaledSigmaPoints, UnscentedKalmanFilter
 from scipy.spatial.transform import Rotation
 
 from misc import wrap_angle, arctan2pos, safe_arcsin, law_of_cosines, angle_components, norm
-from model import reverse_transform, measure, propagate, initial_state, finalize_transform
+from model import reverse_transform, measure, propagate, initial_state, finalize_transform, \
+    state_autocovariance_matrix, measurement_autocovariance_matrix
 from orbit import (eccentric_anomaly_from_mean_anomaly, true_anomaly_from_eccentric_anomaly,
                    distance_from_eccentric_anomaly)
 from photometry import visual_magnitude_from_absolute
@@ -50,8 +51,11 @@ SUN_W = np.radians(3.231307573061241E+02)   # Sun orbit argument of periapsis
 SUN_N = np.radians(8.293546641817494E-02)   # Sun orbit mean motion
 SUN_TP = 2.452714888563351E+06              # Sun orbit time of periapsis JD TDB
 
-DIR_CONC = 1E7  # Precision of the observation direction (concentration parameter of the Fisher distribution)
-MAG_VAR = 0.01  # Variance of the visual magnitude
+# Variance of the observation direction (technically the inverse of the concentration parameter of the Fisher
+# distribution)
+DIR_VAR = 1E-7
+# Variance of the visual magnitude
+MAG_VAR = 1E-2
 
 
 def real_position(t: float, a: float, e: float, i: float, om: float, w: float, n: float, tp: float) -> np.ndarray:
@@ -72,7 +76,7 @@ def gen_times(n: int, min_dist: float, max_dist: float, start: float, rng: np.ra
 
 
 def observe(tgt_pos: np.ndarray, obs_pos: np.ndarray, sun_pos: np.ndarray, hh: float, gg: float,
-            direction_precision: float, vv_var: float, rng: np.random.Generator) -> np.ndarray:
+            dir_var: float, vv_var: float, rng: np.random.Generator) -> np.ndarray:
     tgt_obs_pos = tgt_pos - obs_pos
     tgt_obs_dist = norm(tgt_obs_pos)
 
@@ -80,7 +84,7 @@ def observe(tgt_pos: np.ndarray, obs_pos: np.ndarray, sun_pos: np.ndarray, hh: f
     # Apply Fisher noise to the observation direction
     # https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.vonmises_fisher.html
     # noinspection PyTypeChecker
-    tgt_obs_ray = sp.stats.vonmises_fisher(mu=tgt_obs_ray, kappa=direction_precision, seed=rng).rvs(1)[0]
+    tgt_obs_ray = sp.stats.vonmises_fisher(mu=tgt_obs_ray, kappa=(1.0 / dir_var), seed=rng).rvs(1)[0]
     ra = arctan2pos(tgt_obs_ray[1], tgt_obs_ray[0])
     dec = safe_arcsin(tgt_obs_ray[2])
 
@@ -114,20 +118,20 @@ def main() -> None:
     mm0_hint = 0.0
     om0_hint = TGT_OM
 
-    dir_conc = DIR_CONC
+    dir_var = DIR_VAR
     vv_var = MAG_VAR
 
-    noisy = [observe(t, o, s, TGT_HH, TGT_GG, DIR_CONC, MAG_VAR, rng)
+    noisy = [observe(t, o, s, TGT_HH, TGT_GG, DIR_VAR, MAG_VAR, rng)
              for t, o, s in zip(tgt_pos, obs_pos, sun_pos)]
     prepared = [prepare(obs) for obs in noisy]
 
     naive = [reverse_transform(obs, o, s, hh0, gg0) for obs, o, s in zip(prepared, obs_pos, sun_pos)]
 
-    points = MerweScaledSigmaPoints(13, alpha=1e-3, beta=2, kappa=1)
+    points = MerweScaledSigmaPoints(13, alpha=1E-3, beta=2, kappa=0)
     ukf = UnscentedKalmanFilter(dim_x=13, dim_z=5, dt=None, hx=measure, fx=propagate, points=points)
 
     x0 = initial_state(prepared[0], obs_pos[0], sun_pos[0], a0, e0, i0, n0, hh0, gg0, mm0_hint, om0_hint)
-    pp0 = np.diag([0.5, 0.5, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001])
+    pp0 = state_autocovariance_matrix(x0, 1E-3, 1E-3, 1E-3, 1E-3, 1E-3, 1E-3, 1E-3, 1E-3, 1E-3)
     prev_t = times[0]
     ukf.x = x0
     ukf.P = pp0
@@ -135,24 +139,9 @@ def main() -> None:
     estimates = [finalize_transform(x0)]
     for t, obs, o, s in zip(times[1:], prepared[1:], obs_pos[1:], sun_pos[1:]):
         dt = t - prev_t
-        ukf.Q = 1E-7 * np.eye(13)
-        cos_ra, sin_ra, cos_dec, sin_dec, v = obs
-        ukf.R = np.array([[sin_ra * sin_ra / (dir_conc * cos_dec * cos_dec),
-                           -sin_ra * cos_ra / (dir_conc * cos_dec * cos_dec),
-                           0.0, 0.0, 0.0],
-                          [-sin_ra * cos_ra / (dir_conc * cos_dec * cos_dec),
-                           cos_ra * cos_ra / (dir_conc * cos_dec * cos_dec),
-                           0.0, 0.0, 0.0],
-                          [0.0, 0.0,
-                           sin_dec * sin_dec / dir_conc,
-                           -sin_dec * cos_dec / dir_conc,
-                           0.0],
-                          [0.0, 0.0,
-                           -sin_dec * cos_dec / dir_conc,
-                           cos_dec * cos_dec / dir_conc,
-                           0.0],
-                          [0.0, 0.0, 0.0, 0.0, vv_var]])
+        ukf.Q = state_autocovariance_matrix(ukf.x, 1E-8, 1E-8, 1E-8, 1E-8, 1E-8, 1E-8, 1E-8, 1E-8, 1E-8)
         ukf.predict(dt=dt)
+        ukf.R = measurement_autocovariance_matrix(obs, dir_var, vv_var)
         ukf.update(z=obs, obs_pos=o, sun_pos=s)
         estimates.append(finalize_transform(ukf.x))
         prev_t = t